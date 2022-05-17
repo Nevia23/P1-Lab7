173868f4624f50e6f2a3b34359ffbc9957fa2bf3
Nevia23 <bzaghetto23@gmail.com>
2022-05-17 23:45:19 +0200

    update

--- array.c
+++ array.c
@@ -28,3 +28,16 @@ void count_frequencies (int A[], int dim, int F[], int dimF) {
         F[A[i]] += 1;
     }
 }
+
+void inserisci_in_array(int A[], int start_index, int end_index) {
+
+    int value = A[start_index];
+    int index = start_index;
+
+    while (index != end_index) {
+        A[index] = A[index - 1];
+        index--;
+    }
+    
+    A[end_index] = value;
+}
--- merge_sort.c
+++ merge_sort.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include "array.h"
 #include "merge_sort.h"
 
+void merge(int arr[], int l, int m, int r) {
+
+    int l1 = m + 1;
+
+    if (arr[m] <= arr[l1]) {
+        return;
+    }
+
+    while (l <= m && l1 <= r) {
+
+        if (arr[l] <= arr[l1]) {
+            l++;
+        } else {
+            inserisci_in_array(arr, l1, l);
+        }
+
+        l++; m++; l1++;
+    }
+
+}
+
 void mergeSort(int arr[], int l, int r) {
-    
+
+    int m = l + (r - l)/2;
+
+    if (l < r) {
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
 }
